710f4f4754cf9812ab8feb021fdf4c61c74ba398
JackieLin2004 <linxy20040304@163.com>
2025-02-25 19:10:53 +0800

    Nacos配置中心基本用法

--- services/service-order/src/main/java/com/example/order/controller/OrderController.java
+++ services/service-order/src/main/java/com/example/order/controller/OrderController.java
@@ -3,15 +3,28 @@ package com.example.order.controller;
 import com.example.order.entity.Order;
 import com.example.order.service.OrderService;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope    // 自动刷新注解
 @RestController
 public class OrderController {
 
     @Resource
     OrderService orderService;
+
+    @Value("${order.timeout}")
+    String orderTimeout;
+    @Value("${order.auto-confirm}")
+    String orderAutoConfirm;
+
+    @GetMapping("/config")
+    public String config() {
+        return "timeout: " + orderTimeout + ", auto-confirm: " + orderAutoConfirm;
+    }
 
     // 创建订单
     @GetMapping("/create")
